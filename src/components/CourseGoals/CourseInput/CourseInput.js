cb2579cd9f69eaed531331376fae6be58bc70e90
Julio Diaz <>
2021-05-12 22:15:34 -0500

    Add button

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FromControl = styled.div`
-  margin: 0.5rem 0;
+// const FromControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${comProps => !comProps.invalid ? 'red' : 'black'}
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${comProps => !comProps.invalid ? 'red' : 'black'}
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${comProps => !comProps.invalid ? 'red' : '#ccc'};
-    background: ${comProps => !comProps.invalid ? '#ffd7d7' : 'transparent'}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${comProps => !comProps.invalid ? 'red' : '#ccc'};
+//     background: ${comProps => !comProps.invalid ? '#ffd7d7' : 'transparent'}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -40,6 +40,7 @@ const CourseInput = props => {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
+    console.log(event.target.value)
   };
 
   const formSubmitHandler = event => {
@@ -54,14 +55,14 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FromControl invalid={isValid}
-        // className={!isValid ? 'invalid' : ''}
+      <div
+        className={`${styles['form-control']} ${!isValid ? 'invalid' : ''}`}
         >
         <label >Course Goal</label>
         <input type="text" 
           value={enteredValue}
           onChange={goalInputChangeHandler} />
-      </FromControl>
+      </div>
 
       <Button type="submit">Add Goal</Button>
     </form>
